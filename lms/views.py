e63a003f79077e834231b83ec4ae73f3b9845a71
bledidalipaj <bledikv@gmail.com>
2020-04-13 00:05:19 +0300

    Highlight navigation link based on current page

--- lms/views.py
+++ lms/views.py
@@ -6,10 +6,18 @@ from .models import Patron
 
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'page': 'home'}
+    return render(request, 'home.html', context)
 
 
 class ListPatronsView(ListView):
-	context_object_name = 'patrons'
-	model = Patron
-	template_name = 'patrons.html'
+    context_object_name = 'patrons'
+    model = Patron
+    template_name = 'patrons.html'
+
+    def get_context_data(self, **kwargs):
+        # call the base implementation fist to get a context
+        context = super().get_context_data(**kwargs)
+        # add page variable
+        context['page'] = 'patrons'
+        return context
