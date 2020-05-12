1d19eac11caf60afc12765453d5d17816348a2bc
Bledi Dalipaj <bledikv@gmail.com>
2020-05-13 01:22:36 +0300

    Customize admin

--- lms/admin.py
+++ lms/admin.py
@@ -13,12 +13,56 @@ from .models import (
 )
 
 
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(Checkout)
-admin.site.register(CheckoutHistory)
-admin.site.register(Hold)
-admin.site.register(LibraryBranch)
-admin.site.register(LibraryCard)
-admin.site.register(Patron)
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ('first_name', 'last_name',)
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publish_date', 'status', 'location',)
+    list_editable = ('status',)
+    list_filter = ('status', 'location')
+    search_fields = ('title',)
+
+
+class CheckoutAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'library_asset', 'library_card', 'since', 'until',)
+    search_fields = ('library_asset__title',)
+
+
+class CheckoutHistoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'library_asset', 'library_card',
+                    'checked_out', 'checked_in',)
+    search_fields = ('library_asset__title',)
+
+
+class HoldAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'library_asset', 'library_card', 'hold_placed',)
+    search_fields = ('library_asset__title',)
+
+
+class LibraryBranchAdmin(admin.ModelAdmin):
+    list_display = ('branch_name', 'address', 'telephone', 'open_date',)
+
+
+class LibraryCardAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'patron', 'fees', 'created',)
+    search_fields = ('patron__first_name', 'patron__last_name',)
+
+
+class PatronAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'first_name', 'last_name',
+                    'date_of_birth', 'gender', 'home_library_branch',)
+    list_filter = ('gender', 'home_library_branch__branch_name',)
+
+    search_fields = ('first_name', 'last_name',)
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Checkout, CheckoutAdmin)
+admin.site.register(CheckoutHistory, CheckoutHistoryAdmin)
+admin.site.register(Hold, HoldAdmin)
+admin.site.register(LibraryBranch, LibraryBranchAdmin)
+admin.site.register(LibraryCard, LibraryCardAdmin)
+admin.site.register(Patron, PatronAdmin)
 admin.site.register(Status)
