61af5cf887fd0a0cc2d0710719859ebca81f055f
Bledi Dalipaj <bledikv@gmail.com>
2020-05-16 01:20:25 +0300

    Update asset status after 'Check Out' and after 'Check In'

---
+++ lms/enums.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class StatusEnum(Enum):
+    AVAILABLE = 'AV'
+    CHECKED_OUT = 'CO'
+    LOST = 'LO'
--- lms/signals.py
+++ lms/signals.py
@@ -1,7 +1,8 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
-from .models import Checkout, CheckoutHistory, LibraryCard, Patron
+from .enums import StatusEnum
+from .models import Checkout, CheckoutHistory, LibraryCard, Patron, Status
 
 
 @receiver(post_save, sender=Patron)
@@ -17,3 +18,25 @@ def create_checkout_history(sender, instance, created, **kwargs):
             library_asset=instance.library_asset,
             library_card=instance.library_card
         )
+
+
+@receiver(post_save, sender=Checkout)
+def update_library_asset_status_post_save(sender, instance, created, **kwargs):
+    library_asset = instance.library_asset
+
+    if created:
+        if not library_asset.is_available_to_borrow:
+            updated_status = Status.objects.get(
+                name=StatusEnum.CHECKED_OUT.value)
+            library_asset.status = updated_status
+            library_asset.save()
+
+
+@receiver(post_delete, sender=Checkout)
+def update_library_asset_status_post_delete(sender, instance, **kwargs):
+    library_asset = instance.library_asset
+
+    if library_asset.is_available_to_borrow:
+        updated_status = Status.objects.get(name=StatusEnum.AVAILABLE.value)
+        library_asset.status = updated_status
+        library_asset.save()
