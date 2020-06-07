4dab996b848691c164382449bc18bd23880cf1ee
Bledi Dalipaj <bledikv@gmail.com>
2020-06-08 00:55:34 +0300

    Add user authentication

--- lms_project/urls.py
+++ lms_project/urls.py
@@ -21,6 +21,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('lms.urls')),
+    path('', include('django.contrib.auth.urls')),
 ]
 
 if settings.DEBUG:
